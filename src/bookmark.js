6bc9915cf6359f6d418b8f57edb29af815267ee1
filthiness <filthiness@example.com>
2025-05-23 11:11:19 -0700

    bmark save

--- src/bookmark.js
+++ src/bookmark.js
@@ -29,6 +29,44 @@ export function markToki(i){
     return true;
 }
 
+export class BookmarkManager{
+    constructor(container){
+        this.container = container;
+    }
+
+    getBookmark(){
+        return Array.from(this.container.querySelectorAll('.mecab-token')).findIndex(el => 
+            el.classList.contains('bookmarked')
+        );
+    }
+
+    scrollToBookmark() {
+        this.container.querySelectorAll('.bookmarked').forEach(el => {
+            el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            return true; 
+        });
+        return false;
+    }
+
+    markElement(el) {
+        // Remove any existing bookmarks first
+        this.container.querySelectorAll('.bookmarked').forEach(el => {
+            el.classList.remove('bookmarked');
+        });
+
+        el.classList.add('bookmarked');
+    }
+
+    markToki(i){
+        const tok = this.container.querySelectorAll('.mecab-token')[i]
+
+        if (tok === undefined) return false;
+
+        markElement(tok);
+        return true;
+    }
+}
+
 // ----------------------init------------------------
 
 
